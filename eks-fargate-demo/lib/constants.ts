364e39670486d5725aa1582f67f7b78a09768679
andy <babyandy0111@gmail.com>
2021-04-08 14:18:14 +0800

    eks

---
+++ eks-fargate-demo/lib/constants.ts
@@ -0,0 +1,3 @@
+export const hostedZone: string = "indochat.net";
+export const traefikNamespace: string = "traefik";
+export const traefikBasicAuthString: string = "dXNlcjokYXByMSRYUUZCSWxIaiR2VG8zdTVZYnBSVi95ZXBWbFlHYzYwCgo=";
--- eks-fargate-demo/lib/eks_fargate_demo-stack.ts
+++ eks-fargate-demo/lib/eks_fargate_demo-stack.ts
@@ -2,66 +2,32 @@ import * as cdk from '@aws-cdk/core';
 import * as eks from '@aws-cdk/aws-eks';
 import * as iam from '@aws-cdk/aws-iam';
 import * as ec2 from "@aws-cdk/aws-ec2";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as acm from "@aws-cdk/aws-certificatemanager";
+import { hostedZone } from "./constants";
+import { ingressControllerConfig } from "./traefik/ingress";
+import { dashboardIngressRoute, dashboardMiddleware, dashboardAuth } from "./traefik/dashboard";
+
 
 export class EksFargateDemoStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-        // 這邊就是用kubernetes的yaml to json的方式呈現
-        // const appLabel = {app: "hello-kubernetes"}
-        // const deployment = {
-        //     apiVersion: "apps/v1",
-        //     kind: "Deployment",
-        //     metadata: {name: "hello-kubernetes"},
-        //     spec: {
-        //         replicas: 1,
-        //         selector: {matchLabels: appLabel},
-        //         template: {
-        //             metadata: {labels: appLabel},
-        //             spec: {
-        //                 containers: [
-        //                     {
-        //                         name: "hello-kubernetes",
-        //                         image: "paulbouwer/hello-kubernetes:1.5",
-        //                         ports: [{containerPort: 8080}],
-        //                     },
-        //                 ],
-        //             },
-        //         },
-        //     },
-        // }
-        // const service = {
-        //     apiVersion: "v1",
-        //     kind: "Service",
-        //     metadata: {name: "hello-kubernetes"},
-        //     spec: {
-        //         type: "LoadBalancer",
-        //         ports: [{port: 80, targetPort: 8080}],
-        //         selector: appLabel,
-        //     },
-        // }
+        // Lookup hosted zone
+        const zone = route53.HostedZone.fromLookup(this, "MyCoolHostedZone", {
+            domainName: hostedZone,
+        });
+
+        // Create a wildcard certificate for our hosted zone
+        const cert = new acm.Certificate(this, "MySecureWildcardCert", {
+            domainName: `*.${hostedZone}`,
+            validation: acm.CertificateValidation.fromDns(zone),
+        });
 
         const mastersRole = new iam.Role(this, "adminRole", {
             assumedBy: new iam.AccountRootPrincipal(),
         });
 
-        // Fargate
-        // const cluster = new eks.FargateCluster(this, 'EksFargateCluster', {
-        //     version: eks.KubernetesVersion.V1_19,
-        //     mastersRole: mastersRole,
-        //     outputClusterName: true,
-        //     outputMastersRoleArn: true,
-        //     outputConfigCommand: true,
-        //     vpc: vpc,
-        //     defaultProfile: {
-        //         selectors: [
-        //             {namespace: 'default'},
-        //             {namespace: 'kube-system'},
-        //             {namespace: 'demo', labels: appLabel},
-        //         ],
-        //     }
-        // })
 
         // ec2
         const cluster = new eks.Cluster(this, "EksCluster", {
@@ -72,17 +38,23 @@ export class EksFargateDemoStack extends cdk.Stack {
             clusterName: 'eks-cluster',
         })
 
-        // 用spot, 看價錢
-        // cluster.addAutoScalingGroupCapacity("spot", {
-        //     spotPrice: "0.1094",
-        //     instanceType: new ec2.InstanceType("t3.large"),
-        // });
+        // Pass certificate to Traefik Ingress configuration and add Helm chart
+        ingressControllerConfig.values.service.annotations["service.beta.kubernetes.io/aws-load-balancer-ssl-cert"] = cert.certificateArn;
+        const controller = cluster.addHelmChart("MyTraefikIngress", ingressControllerConfig);
+
+        // Add middleware with dependency to make sure the Ingress is ready
+        const middleware = cluster.addManifest("TraefikMiddleWare", dashboardAuth, dashboardMiddleware);
+        middleware.node.addDependency(controller);
+
+        // Add the Traefik dashboard Ingress route & dependency on middleware
+        const dashboard = cluster.addManifest("TraefikDashboard", dashboardIngressRoute);
+        dashboard.node.addDependency(middleware);
 
         // cluster.addManifest("EksClusterManifest", service, deployment);
 
-        // new cdk.CfnOutput(this, "hello-kubernetes-LoadBalancer", {
-        //     value: cluster.getServiceLoadBalancerAddress("hello-kubernetes"),
-        // });
+        new cdk.CfnOutput(this, "LoadBalancer", {
+            value: hostedZone,
+        });
 
         // new cdk.CfnOutput(this, "hello-k8s-LoadBalancer", {
         //     value: cluster.getServiceLoadBalancerAddress("hello-k8s"),
